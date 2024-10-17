92703a8051708bb2dc8419216351d0420b1f9b45
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2024-10-16 21:59:24 -0400

    added properties and methods to the ToDoList class and added categories module

--- To-Do-List-App/test_to_do_list.py
+++ To-Do-List-App/test_to_do_list.py
@@ -4,8 +4,15 @@ import to_do_item
 
 class TestToDoList(unittest.TestCase):
 
-    def test_getToDoItems(self):
-        self.assertEqual(0, 0)
+    def test_initialize(self):
+        to_do_list_tc1 = to_do_list.ToDoList()
+        self.assertEqual(len(to_do_list_tc1.to_do_items), 0)
+
+    def test_add_item(self):
+        to_do_list_tc2 = to_do_list.ToDoList()
+        to_do_sample = to_do_item.ToDoItem("Code", "Testing", "10/15/2024", "2")
+        to_do_list_tc2.add_to_do(to_do_sample)
+        self.assertEqual(len(to_do_list_tc2.to_do_items), 1)
 
 if __name__ == '__main__':
     unittest.main()
--- To-Do-List-App/to_do_list.py
+++ To-Do-List-App/to_do_list.py
@@ -1,15 +1,21 @@
+from contextlib import nullcontext
+
 
 class ToDoList(object):
-    def __init__(self, to_do_items):
-        self.to_do_items = []
-        pass
 
-    def add_to_do(self):
-        pass
+    def __init__(self, to_do_items, title, category, to_do_list_id):
+        self.to_do_items = {}
+        self.title = title
+        self.category = category
+        self.to_do_list_id = id
 
-    def edit_to_do(self):
-        pass
+    def add_to_do(self, to_do_item, to_do_id):
+        self.to_do_items[to_do_id] = to_do_item
 
-    def delete_to_do(self):
-        pass
+    @staticmethod
+    def edit_to_do(self, to_do_item):
+        return to_do_item
+
+    def delete_to_do(self, to_do_id):
+        del self.to_do_items[to_do_id]
 
