388612f338cd7fb6f29687252e2193fb83cd108e
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2024-11-07 00:15:45 -0500

    refactored the beginning user input process for adding new list, changed list manager class to array of list objects

--- To-Do-List-App/list_manager.py
+++ To-Do-List-App/list_manager.py
@@ -5,11 +5,7 @@ from to_do_list import ToDoList
 
 class ListManager:
 
-    all_lists = {}
+    all_lists = []
 
     def __init__(self):
         pass
-
-    @classmethod
-    def add_list(cls, to_do_list, list_title):
-        cls.all_lists[list_title] = to_do_list
--- To-Do-List-App/main.py
+++ To-Do-List-App/main.py
@@ -10,39 +10,31 @@ def display_lists(lists):
     if len(lists) == 0:
         print("Empty")
     for single_list in lists:
-        print("-" + " " + single_list)
+        print("-" + " " + single_list.title)
 
 def query_list():
-    selected_category = input("Type list you want to select or type a new list to start creating: ")
-    if selected_category in ListManager.all_lists:
-        # not expecting string for key
-        select_list(selected_category)
+    selected_list = input("Select list by number or enter anything to start a new to do list: ")
+    if selected_list.isdigit():
+        print("picked a list")
     else:
-        create_new_list(selected_category)
-
-
-def add_to_dos(curr_list):
-    description_inquiry = input("Please add to do description or enter /f to finish: ")
-    if description_inquiry != "/f":
-
-        curr_to_do = to_do_item.ToDoItem()
-        curr_list.add_to_do()
+        create_new_list()
 
 
 
-def create_new_list(title_input):
-    print("creating new list: " + title_input)
+# def add_to_dos(curr_list):
+#     description_inquiry = input("Please add to do description or enter /f to finish: ")
+#     if description_inquiry != "/f":
+#
+#         curr_to_do = to_do_item.ToDoItem()
+#         curr_list.add_to_do()
+
+
+
+def create_new_list():
+    title_input = input("Please enter to do list title: ")
     now = datetime.now()
-    add_to_do_inquiry = input("Add To Dos Now? (y/n): ")
     new_list = to_do_list.ToDoList({}, title_input)
-    if add_to_do_inquiry == "y":
-        add_to_dos(new_list)
-
-    if to_do_input.lower() != "exit":
-        new_list.add_to_do(to_do_input, new_list.to_do_list_id)
-
-
-    ListManager.add_list(new_list, new_list.title)
+    ListManager.all_lists.append(new_list)
     print("Displaying all to do lists")
     display_lists(ListManager.all_lists)
 
@@ -56,7 +48,7 @@ def main():
     print("called main")
     display_lists(ListManager.all_lists)
     query_list()
-    query_list()
+    #query_list()
 
 if __name__ == '__main__':
     main()
