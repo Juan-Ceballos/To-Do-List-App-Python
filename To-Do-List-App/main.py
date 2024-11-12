73a8057ccefaedf19612814761757f940dbe9d61
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2024-11-12 09:49:53 -0500

    list deletion function started

--- To-Do-List-App/main.py
+++ To-Do-List-App/main.py
@@ -15,22 +15,55 @@ def display_lists(lists):
     for num in range(num_to_dos):
         print(f"{num + 1} {lists[num].title}")
 
+def create_new_list():
+    now = datetime.now()
+    title_input = input("Please enter to do list title: ")
+    new_list = to_do_list.ToDoList({}, title_input)
+    new_list.to_do_items = add_to_dos()
+    ListManager.all_lists.append(new_list)
+    print("Displaying all to do lists")
+    display_lists(ListManager.all_lists)
+    print("---")
+
+
+def select_list():
+    select_query = input("Please select list number you want to select or type and enter [quit] to go back")
+    if select_query.isdigit():
+        selected_digit = int(select_query) - 1
+        if selected_digit > len(ListManager.all_lists) or selected_digit < 0:
+            print("List does not exist going back to main menu")
+        selected_list = ListManager.all_lists[int(select_query) - 1]
+        for to_do in selected_list.to_do_items:
+            print(f"Here are all your to dos in list {selected_list.title}:")
+            print(to_do.description)
+    elif select_query == "quit":
+        print("Going back to main menu")
+    else:
+        print("Command or input not recognized going back to main menu")
+
+def delete_list():
+    del_query = input("Please type and enter the number of the list to delete")
+    if del_query.isdigit():
+        del_num = int(del_query)
+        ListManager.all_lists.pop(del_num)
+    else:
+        print("do not recognize input going back to main list")
+
 def query_list():
     done = False
     while not done:
-        selected_list = input("Select list by number or enter anything to start a new to do list, DONE to exit: ")
-        if selected_list == "DONE":
-            done = True
-            print("closing program")
-        elif selected_list.isdigit():
-            all_list_count = len(ListManager.all_lists)
-            if int(selected_list) > all_list_count:
-                print("List does not exist")
-                query_list()
-            else:
-                select_list(int(selected_list))
-        else:
-            create_new_list()
+        selected_list = input("1) Press enter key to start a new list\n2) Type and enter [sel] to pick a list"
+                              "\n3) Type and enter [del] to delete a list\n4) Type and enter [quit] to exit\n")
+        match selected_list:
+            case "quit":
+                done = True
+            case "":
+                create_new_list()
+            case "sel":
+                select_list()
+            case "del":
+                delete_list()
+
 
 
 
@@ -45,23 +78,6 @@ def add_to_dos():
         else:
             done = True
     return to_do_inputs
-
-
-
-def create_new_list():
-    now = datetime.now()
-    title_input = input("Please enter to do list title: ")
-    new_list = to_do_list.ToDoList({}, title_input)
-    new_list.to_do_items = add_to_dos()
-    ListManager.all_lists.append(new_list)
-    print("Displaying all to do lists")
-    display_lists(ListManager.all_lists)
-
-def select_list(num_input):
-    current_list = ListManager.all_lists[num_input - 1]
-    print(current_list.title)
-    for list_num, item in enumerate(current_list.to_do_items):
-        print(f"{list_num + 1} {item.description}")
 
 def main():
     print("called main")
